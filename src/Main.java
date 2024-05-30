352dc93587f0d317201ff2fcddf3b97ade282a47
isaacquinapallo <isaac.quinapallo@epn.edu.ec>
2024-05-29 20:48:09 -0500

    Comentario1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //Primera Parte
+        Hoteles hotel1=new Hoteles(20,"La Mar","6 de Diciembre",4.5,10);
+        Hoteles hotel2=new Hoteles(30,"La Paz","La Marin",4,10);
+        Hoteles hotel3=new Hoteles(40,"La Rosa","Andres de Zuñiga",3.5,10);
+        Hoteles hotel4=new Hoteles(50,"La Casa","Gaspar de Villaroel",5,10);
+        Hoteles hotel5=new Hoteles(60,"La Tabla","El Montufar",2.5,10);
+        //Generar dos objetos con valores nulos
+        Hoteles hotel6=new Hoteles();
+        Hoteles hotel7=new Hoteles();
+        //Settear nombres
+        hotel6.setNombre("Colon");
+        hotel6.setNombre("Colon");
         System.out.println("Menú principal");
         System.out.println(" ");
     }
